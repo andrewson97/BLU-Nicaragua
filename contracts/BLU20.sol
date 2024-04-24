a02277ed9adf015c0565c2e86136f2a0889b9e46
Andrewson Croos <acroos@favoritemedium.com>
2024-04-25 02:16:57 +0530

    Modify to match Nicaragua

--- contracts/BLU20.sol
+++ contracts/BLU20.sol
@@ -2,54 +2,71 @@
 //SPDX-License-Identifier: UNLICENSED
 
 
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.17;
 
 import "./ERC20.sol";
 
 contract Blue20 is ERC20  {
     address payable public owner;
+    uint256 varientId = 0;
 
-    struct TokenMetadata {
-            string volume_ml;
-            string date;
+    struct VarientMetadata {
             string facility_id;
             string sensor_id;
+            string date;
+            uint256 varient_supply;
     }
 
-    mapping(uint256 => TokenMetadata) public tokenMetadata;
+    mapping(uint256 => VarientMetadata) public varientMetadata;
 
 
-    constructor() ERC20 ("BlueToken","BLUE"){
+    constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20 (name, symbol){
         owner = payable(msg.sender);
+        _mint(msg.sender, initialSupply);
     }
-    
-    
-    function issueToken(string memory _volume_ml, string memory _date, string memory _facility, string memory _sensor) public {
-        if (msg.sender == owner){
-            uint256 tokenId = totalSupply() + 1;
-            _mint(msg.sender,1);
 
-            tokenMetadata[tokenId] = TokenMetadata({
-            volume_ml: _volume_ml,
-            date: _date,
+    function addVarient(string memory _facility, string memory _sensor, string memory _date) public {
+        require(msg.sender == owner, "Only the owner can add varient");
+        varientId = varientId + 1;
+
+        varientMetadata[varientId] = VarientMetadata({
             facility_id: _facility,
-            sensor_id: _sensor
-
+            sensor_id: _sensor,
+            date: _date,
+            varient_supply: 0
         });
-            
-        }
-        else{
-            require(msg.sender == owner, "Only owner of the contract can Mint tokens for now");
-            return;
-        }
     }
     
+    function increaseTotalSupply(uint256 _varientId, uint256 _amount) public {
+        require(msg.sender == owner, "Only the owner can increase total supply");
+        require(_varientId <= varientId, "Varient with this ID does not exist");
 
-    function getTokenMetadata(uint256 tokenId) public view returns (string memory flow, string memory date, string memory facility_id, string memory sensor_id) {
-    require(tokenId <= totalSupply(), "Token with this ID does not exist");
+        VarientMetadata storage varient = varientMetadata[_varientId];
+        varient.varient_supply += _amount;
+
+        // Mint new tokens and add them to the total supply
+        _mint(msg.sender, _amount);
+    }
+
+    function issueToken(uint256 _volume_l, address _to) public {
+        require(msg.sender == owner, "Only the owner can mint tokens");
+
+        // Increase allowance by _volume_ml to _to
+        _approve(msg.sender, _to, allowance(msg.sender, _to) + _volume_l);
+    }
+
     
-    TokenMetadata storage metadata = tokenMetadata[tokenId];
-    return (metadata.volume_ml, metadata.date,metadata.facility_id,metadata.sensor_id);
-}
+
+    function getVarientMetadata(uint256 _varientId) public view returns (string memory facility_id, string memory sensor_id, string memory date, uint256 varient_supply) {
+        require(_varientId <= varientId, "Varient with this ID does not exist");
+
+        VarientMetadata storage metadata = varientMetadata[_varientId];
+        return (metadata.facility_id, metadata.sensor_id, metadata.date, metadata.varient_supply);
+    }
+
+    function getNumberOfVariants() public view returns (uint256) {
+        return varientId;
+    }
+
 
 }
