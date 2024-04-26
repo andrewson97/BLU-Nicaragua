e660981d56adc2931d33e0ff50641ab2c7e2e258
Andrewson Croos <acroos@favoritemedium.com>
2024-04-26 08:26:13 +0530

    Add Deploy script

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,16 +6,25 @@ require("@nomicfoundation/hardhat-verify");
 module.exports = {
   solidity: "0.8.24",
   networks: {
-    // etherlinkTest: {
-    //   url: "https://node.ghostnet.etherlink.com",
-    //   accounts: [process.env.NMY_PRIVATE_KEY],
-    // },
+    etherlinkTest: {
+      url: "https://node.ghostnet.etherlink.com",
+      accounts: [process.env.MY_PRIVATE_KEY],
+    },
     sepolia:{
       url: process.env.INFURA_SEPOLIA_URL,
       accounts: [process.env.MY_PRIVATE_KEY],
+    },
+    polygonAmoy: {
+      url: process.env.POLYGON_AMOY_RPC,
+      accounts: [process.env.MY_PRIVATE_KEY],
+      gasPrice: "auto",
     }
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY
+    apiKey: {
+      polygonAmoy: process.env.INFURA_API_KEY,
+      //sepolia: process.env.SEPOLIA_API,
+      etherlinkTest: process.env.ETHERSCAN_API_KEY,
+    }
   }
 };
--- ignition/modules/BLU20.js
+++ ignition/modules/BLU20.js
@@ -3,7 +3,7 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 module.exports = buildModule("Blue20Module", (m) => {
-  const token = m.contract("Blue20", []);
+  const token = m.contract("Blue20", ["BLU20", "BLU20", 10000]);
 
   return { token };
 });
